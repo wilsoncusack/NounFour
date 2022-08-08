02e600f2832b3d7e56a4af5ec491447eb05fa022
Wilson Cusack <cusackw@gmail.com>
2022-08-07 20:43:03 -0400

    WIP

--- src/Contract.sol
+++ src/Contract.sol
@@ -1,4 +1,9 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.13;
 
-contract Contract {}
+import {NounsToken} from 'nouns-monorepo/packages/nouns-contracts/contracts/NounsToken.sol';
+import {ERC721, ERC721TokenReceiver} from 'solmate/tokens/ERC721.sol';
+
+contract PartsOfNoun is ERC721, ERC721TokenReceiver {
+
+}
