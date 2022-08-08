033203b27eda02d5f6512e7d0606e9537ab14a5b
Wilson Cusack <cusackw@gmail.com>
2022-08-07 23:33:23 -0400

    forge fmt, license, re order funcs

--- src/NounFour.sol
+++ src/NounFour.sol
@@ -1,15 +1,28 @@
-// SPDX-License-Identifier: UNLICENSED
+// SPDX-License-Identifier: MIT
 pragma solidity ^0.8.13;
 
-import {NounsToken, INounsSeeder} from 'nouns-monorepo/packages/nouns-contracts/contracts/NounsToken.sol';
-import {NounsDescriptorV2, ISVGRenderer} from 'nouns-monorepo/packages/nouns-contracts/contracts/NounsDescriptorV2.sol';
-import {NounsDAOLogicV1} from 'nouns-monorepo/packages/nouns-contracts/contracts/governance/NounsDAOLogicV1.sol';
-import {NounsDAOStorageV1} from 'nouns-monorepo/packages/nouns-contracts/contracts/governance/NounsDAOInterfaces.sol';
-import {ERC721, ERC721TokenReceiver} from 'solmate/tokens/ERC721.sol';
-import {Base64} from 'base64/base64.sol';
-import {Strings} from 'openzeppelin-contracts/contracts/utils/Strings.sol';
+import {
+    NounsToken,
+    INounsSeeder
+} from "nouns-monorepo/packages/nouns-contracts/contracts/NounsToken.sol";
+import {
+    NounsDescriptorV2,
+    ISVGRenderer
+} from "nouns-monorepo/packages/nouns-contracts/contracts/NounsDescriptorV2.sol";
+import {NounsDAOLogicV1} from
+    "nouns-monorepo/packages/nouns-contracts/contracts/governance/NounsDAOLogicV1.sol";
+import {NounsDAOStorageV1} from
+    "nouns-monorepo/packages/nouns-contracts/contracts/governance/NounsDAOInterfaces.sol";
+import {ERC721, ERC721TokenReceiver} from "solmate/tokens/ERC721.sol";
+import {Base64} from "base64/base64.sol";
+import {Strings} from "openzeppelin-contracts/contracts/utils/Strings.sol";
 
-enum NounPart {body, accessory, head, glasses}
+enum NounPart {
+    body,
+    accessory,
+    head,
+    glasses
+}
 
 struct PartInfo {
     NounPart part;
@@ -18,90 +31,61 @@ struct PartInfo {
 
 contract NounFour is ERC721("NounFour", "N4"), ERC721TokenReceiver {
     NounsToken immutable nouns;
-    NounsDescriptorV2 immutable public descriptors;
-    NounsDAOLogicV1 immutable public nounsDAO; 
+    NounsDescriptorV2 public immutable descriptors;
+    NounsDAOLogicV1 public immutable nounsDAO;
     // tokenId => partInfo
     mapping(uint256 => PartInfo) public partInfo;
     // tokenId => proposalId => support
     mapping(uint256 => mapping(uint256 => uint8)) public voteReceipt;
     uint256 _nonce;
 
-    string private constant _SVG_START_TAG = '<svg width="320" height="320" viewBox="0 0 320 320" xmlns="http://www.w3.org/2000/svg" shape-rendering="crispEdges">';
-    string private constant _SVG_END_TAG = '</svg>';
+    string private constant _SVG_START_TAG =
+        '<svg width="320" height="320" viewBox="0 0 320 320" xmlns="http://www.w3.org/2000/svg" shape-rendering="crispEdges">';
+    string private constant _SVG_END_TAG = "</svg>";
 
-    constructor(NounsToken _nouns, NounsDescriptorV2 _descriptors, NounsDAOLogicV1 _nounsDAO) {
+    constructor(
+        NounsToken _nouns,
+        NounsDescriptorV2 _descriptors,
+        NounsDAOLogicV1 _nounsDAO
+    ) {
         nouns = _nouns;
         descriptors = _descriptors;
         nounsDAO = _nounsDAO;
     }
 
-    function tokenURI(uint256 id) public view override returns (string memory) {
-        (string memory partType, string memory image) = partSVG(id);
-
-        string memory name = string.concat(partType, ' of Noun ', Strings.toString(partInfo[id].nounId));
-
-        return string.concat(
-            'data:application/json;base64,',
-            Base64.encode(
-                bytes(
-                    abi.encodePacked('{"name":"', name, '", "description":"', '', '", "image": "', 'data:image/svg+xml;base64,', Base64.encode(bytes(image)), '"}')
-                )
-            )
-        );
-    }
-
     error OnlyOwner();
 
-    function castVote(uint256 tokenId, uint256 proposalId, uint8 support) external {
+    function castVote(uint256 tokenId, uint256 proposalId, uint8 support)
+        external
+    {
         if (msg.sender != ownerOf(tokenId)) {
             revert OnlyOwner();
         }
         /// TODO
     }
 
-    function conveyVote(uint256 nounId, uint256 body, uint256 accessory, uint256 head, uint256 glasses) external {
+    function conveyVote(
+        uint256 nounId,
+        uint256 body,
+        uint256 accessory,
+        uint256 head,
+        uint256 glasses
+    )
+        external
+    {
         /// TODO, convey vote of noun four to the DAO
-    }
-
-    function partSVG(uint256 id) public view returns (string memory partType, string memory svg) {
-        PartInfo storage info = partInfo[id];
-        (uint48 background, uint48 body, uint48 accessory, uint48 head, uint48 glasses) = nouns.seeds(info.nounId);
-        
-        bytes memory partBytes;
-        if (info.part == NounPart.body) {
-            partType = "body";
-            partBytes = descriptors.art().bodies(body);
-        } else if (info.part == NounPart.accessory) {
-            partType = "accessory";
-            partBytes = descriptors.art().accessories(accessory);
-        } else if(info.part == NounPart.head) {
-            partType = "head";
-            partBytes = descriptors.art().heads(head);
-        } else if(info.part == NounPart.glasses) {
-            partType = "glasses";
-            partBytes = descriptors.art().glasses(glasses);
-        }
-
-        ISVGRenderer.Part memory svgPart = ISVGRenderer.Part({ image: partBytes, palette: _getPalette(partBytes) });
-
-        string memory renderedSVGPart = descriptors.renderer().generateSVGPart(svgPart);
-
-        svg = string.concat(
-            _SVG_START_TAG,
-            '<rect width="100%" height="100%" fill="#', descriptors.art().backgrounds(background), '" />',
-            renderedSVGPart,
-            _SVG_END_TAG
-        );
     }
 
     function withdrawNoun(
         address to,
         uint256 nounId,
-        uint256 body, 
-        uint256 accessory, 
-        uint256 head, 
+        uint256 body,
+        uint256 accessory,
+        uint256 head,
         uint256 glasses
-    ) external {
+    )
+        external
+    {
         _burnPart(body, nounId, NounPart.body);
         _burnPart(body, nounId, NounPart.accessory);
         _burnPart(body, nounId, NounPart.head);
@@ -116,14 +100,49 @@ contract NounFour is ERC721("NounFour", "N4"), ERC721TokenReceiver {
         address from,
         uint256 tokenId,
         bytes calldata
-    ) external override returns (bytes4) {
-        if (msg.sender != address(nouns)){
+    )
+        external
+        override
+        returns (bytes4)
+    {
+        if (msg.sender != address(nouns)) {
             revert NounsOnly();
         }
-        
+
         _mintNounParts(from, tokenId);
 
         return ERC721TokenReceiver.onERC721Received.selector;
+    }
+
+    function tokenURI(uint256 id)
+        public
+        view
+        override
+        returns (string memory)
+    {
+        (string memory partType, string memory image) = partSVG(id);
+
+        string memory name = string.concat(
+            partType, " of Noun ", Strings.toString(partInfo[id].nounId)
+        );
+
+        return string.concat(
+            "data:application/json;base64,",
+            Base64.encode(
+                bytes(
+                    abi.encodePacked(
+                        '{"name":"',
+                        name,
+                        '", "description":"',
+                        "",
+                        '", "image": "',
+                        "data:image/svg+xml;base64,",
+                        Base64.encode(bytes(image)),
+                        '"}'
+                    )
+                )
+            )
+        );
     }
 
     function _mintNounParts(address to, uint256 nounId) internal {
@@ -135,12 +154,16 @@ contract NounFour is ERC721("NounFour", "N4"), ERC721TokenReceiver {
         _nonce = cur;
     }
 
-    function _mintPart(address to, uint256 nounId, uint256 tokenId, NounPart part) internal {
+    function _mintPart(
+        address to,
+        uint256 nounId,
+        uint256 tokenId,
+        NounPart part
+    )
+        internal
+    {
         _safeMint(to, tokenId);
-        partInfo[tokenId] = PartInfo({
-            part: part,
-            nounId: uint248(nounId)
-        });
+        partInfo[tokenId] = PartInfo({part: part, nounId: uint248(nounId)});
     }
 
     error MustBeOwnerOrApproved();
@@ -161,10 +184,59 @@ contract NounFour is ERC721("NounFour", "N4"), ERC721TokenReceiver {
             revert NounIdDoesNotMatch();
         }
 
-        _burn(id);    
+        _burn(id);
     }
 
-    function _getPalette(bytes memory part) private view returns (bytes memory) {
+    function partSVG(uint256 id)
+        public
+        view
+        returns (string memory partType, string memory svg)
+    {
+        PartInfo storage info = partInfo[id];
+        (
+            uint48 background,
+            uint48 body,
+            uint48 accessory,
+            uint48 head,
+            uint48 glasses
+        ) = nouns.seeds(info.nounId);
+
+        bytes memory partBytes;
+        if (info.part == NounPart.body) {
+            partType = "body";
+            partBytes = descriptors.art().bodies(body);
+        } else if (info.part == NounPart.accessory) {
+            partType = "accessory";
+            partBytes = descriptors.art().accessories(accessory);
+        } else if (info.part == NounPart.head) {
+            partType = "head";
+            partBytes = descriptors.art().heads(head);
+        } else if (info.part == NounPart.glasses) {
+            partType = "glasses";
+            partBytes = descriptors.art().glasses(glasses);
+        }
+
+        ISVGRenderer.Part memory svgPart =
+            ISVGRenderer.Part({image: partBytes, palette: _getPalette(partBytes)});
+
+        string memory renderedSVGPart =
+            descriptors.renderer().generateSVGPart(svgPart);
+
+        svg = string.concat(
+            _SVG_START_TAG,
+            '<rect width="100%" height="100%" fill="#',
+            descriptors.art().backgrounds(background),
+            '" />',
+            renderedSVGPart,
+            _SVG_END_TAG
+        );
+    }
+
+    function _getPalette(bytes memory part)
+        private
+        view
+        returns (bytes memory)
+    {
         return descriptors.art().palettes(uint8(part[0]));
     }
 }
--- test/NounFour.t.sol
+++ test/NounFour.t.sol
@@ -1,13 +1,15 @@
-// SPDX-License-Identifier: UNLICENSED
+// SPDX-License-Identifier: MIT
 pragma solidity ^0.8.13;
 
 import "forge-std/Test.sol";
-import {NounsToken} from 'nouns-monorepo/packages/nouns-contracts/contracts/NounsToken.sol';
-import {NounsDescriptorV2} from 'nouns-monorepo/packages/nouns-contracts/contracts/NounsDescriptorV2.sol';
-import {NounsDAOLogicV1} from 'nouns-monorepo/packages/nouns-contracts/contracts/governance/NounsDAOLogicV1.sol';
+import {NounsToken} from
+    "nouns-monorepo/packages/nouns-contracts/contracts/NounsToken.sol";
+import {NounsDescriptorV2} from
+    "nouns-monorepo/packages/nouns-contracts/contracts/NounsDescriptorV2.sol";
+import {NounsDAOLogicV1} from
+    "nouns-monorepo/packages/nouns-contracts/contracts/governance/NounsDAOLogicV1.sol";
 
-
-import {NounFour} from 'src/NounFour.sol';
+import {NounFour} from "src/NounFour.sol";
 
 /// TODO TEST ME!!!!
 
@@ -18,19 +20,20 @@ contract NounFourTest is Test {
         NounsDescriptorV2(0x6229c811D04501523C6058bfAAc29c91bb586268),
         NounsDAOLogicV1(0xa43aFE317985726E4e194eb061Af77fbCb43F944)
     );
+
     function setUp() public {}
 
     function testExample() public {
         address owner = nouns.ownerOf(1);
         vm.prank(owner);
         nouns.safeTransferFrom(owner, address(nounFour), 1);
-        emit log_string('body \n');
+        emit log_string("body \n");
         emit log_string(nounFour.tokenURI(0));
-        emit log_string('accessory \n');
+        emit log_string("accessory \n");
         emit log_string(nounFour.tokenURI(1));
-        emit log_string('head \n');
+        emit log_string("head \n");
         emit log_string(nounFour.tokenURI(2));
-        emit log_string('glasses \n');
+        emit log_string("glasses \n");
         emit log_string(nounFour.tokenURI(3));
     }
 }
